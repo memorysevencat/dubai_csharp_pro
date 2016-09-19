fb5d1c88ece0372e5faeda9c22c9e0289d60f418
LiuQingNice <81424640@qq.com>
2016-09-19 10:43:18 +0800

    Create

---
+++ DuBai_Pro/DuBai_Pro/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DuBai_Pro.MvcApplication" Language="C#" %>
---
+++ DuBai_Pro/DuBai_Pro/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DuBai_Pro.Startup))]
+namespace DuBai_Pro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
